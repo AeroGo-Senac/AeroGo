8a34602c71e85b2963aad2978d7807853e4cf410
EduardoPH <eduardo.mendess@icloud.com>
2025-04-01 17:17:48 -0300

    Ajustes tela adm incompleto

---
+++ src/app/components/adminform/adminform.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
+
+@Component({
+  selector: 'app-adminform',
+  imports: [HeaderComponent],
+  templateUrl: './adminform.component.html',
+  styleUrl: './adminform.component.css'
+})
+export class AdmformComponent {
+
+}
