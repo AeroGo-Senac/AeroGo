8d21a84fca33f494fd15cb8291a6761dd96c8d91
Maciel vinicius da costa <macielvinicius500@gmail.com>
2025-04-13 20:47:46 -0300

    add documents profile screen

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,15 +1,16 @@
 import { Component } from '@angular/core';
-import { LucideAngularModule } from 'lucide-angular';
+import { Import, LucideAngularModule } from 'lucide-angular';
 import { HeaderComponent } from '../../components/header/header.component';
+import { MyDocumentsComponent } from '../../components/my-documents/my-documents.component';
 import { MyProfilleComponent } from '../../components/my-profille/my-profille.component';
 
 @Component({
   selector: 'app-profile',
-  imports: [HeaderComponent, MyProfilleComponent, LucideAngularModule],
+  imports: [HeaderComponent, MyProfilleComponent, LucideAngularModule, MyDocumentsComponent],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
 
 export class ProfileComponent {
-
+  
 }
