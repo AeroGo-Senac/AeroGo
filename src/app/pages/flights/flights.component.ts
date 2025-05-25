a4033681e8fde986854855a49d342ee8cc1ebc30
Maciel vinicius da costa <macielvinicius500@gmail.com>
2025-05-25 13:25:19 -0300

    ajuster flight

--- src/app/pages/flights/flights.component.ts
+++ src/app/pages/flights/flights.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { FlightCardComponent } from "../../components/flight-card/flight-card.component";
 import { HeaderComponent } from "../../components/header/header.component";
+import { FilterComponent } from '../../components/filter/filter.component'; 
 
 @Component({
   selector: 'app-flights',
-  imports: [FlightCardComponent, HeaderComponent],
+  imports: [FlightCardComponent, HeaderComponent, FilterComponent],
   templateUrl: './flights.component.html',
   styleUrl: './flights.component.css'
 })
